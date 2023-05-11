0ab84986f889a3e6595082e44d13892159d10936
Robbert Broersma <git@robbertbroersma.nl>
2023-05-11 13:03:55 +0200

    build: update to latest versions

---
+++ packages/component-index/rollup.config.mjs
@@ -0,0 +1,22 @@
+import typescript from "@rollup/plugin-typescript";
+import filesize from "rollup-plugin-filesize";
+import packageJson from "./package.json" assert { type: "json" };
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+    },
+  ],
+  plugins: [typescript(), filesize()],
+};
