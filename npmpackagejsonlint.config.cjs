b84f403b65e4fe1f9b7668c73c54b0da8a10dfc6
matijs <matijs@gmail.com>
2025-04-22 21:49:23 +0200

    chore: align npm-package-json-lint config
    Bring the npm-package-json-lint configuration in line with other NL
    Design System repositories:
    
    - More compact configuration
    - Use npmpackagejsonlint.config.cjs instead of
      .npmpackagejsonlintrc.json
    - Fix package.json's

---
+++ npmpackagejsonlint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    'valid-values-license': ['error', ['EUPL-1.2']],
+    'valid-values-name-scope': ['error', ['@nl-design-system']],
+    'no-caret-version-dependencies': 'error',
+    'no-caret-version-devDependencies': 'error',
+    'no-tilde-version-dependencies': 'error',
+    'no-tilde-version-devDependencies': 'error',
+    'require-repository-directory': 'error',
+  },
+  overrides: [],
+};
